b6135b49f81c76c9cec54e117aa0c25847228c0f
zyunx <zyunone@163.com>
2016-06-02 13:26:25 +0000

    1.5.4 word counting

---
+++ 01tutorial/wc.c
@@ -0,0 +1,27 @@
+/* wc.c - count character, word, line */
+#include <stdio.h>
+
+#define     IN      1
+#define     OUT     0
+
+/* count lines, words, lines in input */
+main()
+{
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF) {
+        ++nc;
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\n' || c == '\t') {
+            state = OUT;
+        } else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+
+    printf("%d %d %d\n", nl, nw, nc);
+}
