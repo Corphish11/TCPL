129dc33a296e660438d6c3bf804a11b87bf525f5
zyunx <zyunone@163.com>
2016-06-03 00:11:27 +0000

    Exercise 1-16

---
+++ 01tutorial/getline2.c
@@ -0,0 +1,58 @@
+/* getline.c - find the longest line */
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int my_getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+main()
+{
+    int len;                    /* current line length */
+    int max;                    /* maximum length seen so far */
+    char line[MAXLINE];          /* current input line */
+    char longest[MAXLINE];       /* longest line saved here */
+
+    max = 0;
+    while ((len = my_getline(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+
+    if (max > 0)
+        printf("%s\nlenth: %d\n", longest, max);
+
+    return 0;
+}
+
+/* getline: read a line into 's', return length */
+int my_getline(char s[], int lim)
+{
+    int c, i;
+    int len;
+
+    for (i = 0; i < lim-1 && (c=getchar())!=EOF  && c!='\n'; ++i)
+        s[i] = c;
+
+    if (c == '\n')
+        s[i++] = c;
+    s[i] = '\0';
+
+    len = i;
+    if (c != '\n')
+        while ((c = getchar()) != EOF && c != '\n')
+            len++;
+
+    return len; 
+}
+
+/* copy: copy 'from' into 'to'; assume 'to' is big enough */
+void copy(char to[], char from[])
+{
+    int i;
+    
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
