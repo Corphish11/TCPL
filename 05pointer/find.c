9623c2b622600ab7f8f8f2360361171deac18b06
zyunx <zyunone@163.com>
2016-06-14 21:49:14 +0000

    find.c v3

--- 05pointer/find.c
+++ 05pointer/find.c
@@ -9,16 +9,37 @@ int my_getline(char *line, int lim);
 main(int argc, char *argv[])
 {
 	char line[MAXLINE];
-	int found = 0;
+	int found = 0, lineno = 0;
+	int except = 0, number = 0;
+	int c;
 
-	if (argc != 2)
-		printf("Usage: find pattern\n");
+	while (--argc > 0 && (*++argv)[0] == '-')
+		while (c = *++argv[0])
+			switch (c) {
+			case 'x':
+				except = 1;
+				break;
+			case 'n':
+				number = 1;
+				break;
+			default:
+				printf("Usage: find -x -n pattern\n");
+				argc = 0;
+				found = -1;
+				break;
+			}
+	if (argc != 1)
+		printf("Usage: find -x -n pattern\n");
 	else
-		while (my_getline(line, MAXLINE) > 0)
-			if (strstr(line, argv[1]) != NULL) {
+		while (my_getline(line, MAXLINE) > 0) {
+			lineno++;
+			if ((strstr(line, *argv) != NULL) != except) {
+				if (number)
+					printf("%d:", lineno);
 				printf("%s", line);
 				found++;
 			}
+		}
 
 	return found;
 }
